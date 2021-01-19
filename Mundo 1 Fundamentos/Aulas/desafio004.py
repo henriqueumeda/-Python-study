db4fa0360fb4a21d0afb47a33e68e14c646d5328
Henrique Umeda <issamuumeda@gmail.com>
2021-01-19 18:39:11 -0300

    Update desafio004.py

--- Mundo 1 Fundamentos/Aulas/desafio004.py
+++ Mundo 1 Fundamentos/Aulas/desafio004.py
@@ -1,4 +1,4 @@
 primeiroNumero = int(input('Primeiro número '))
 segundoNumero = int(input('Segundo número '))
 soma = primeiroNumero + segundoNumero
-print('A soma é {}'.format(soma))
+print('A soma entre {} e {} é {}'.format(primeiroNumero, segundoNumero, soma))

f71325c74870cefff16b52c966e11edb5c2a2edd
Henrique Umeda <issamuumeda@gmail.com>
2021-01-20 20:44:52 -0300

    Create desafio008.py

---
+++ Mundo 1 Fundamentos/Desafios/desafio008.py
@@ -0,0 +1,4 @@
+metros = float(input('Digite um valor (somente números) em metros: '))
+centimetros = metros * 100
+milimetros = metros * 1000
+print('{}m = {}cm = {}mm'.format(metros, centimetros, milimetros))

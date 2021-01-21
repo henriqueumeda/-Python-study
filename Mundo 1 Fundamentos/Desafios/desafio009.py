20e140162453e709f283fff64acf015ccd36a2d0
Henrique Umeda <issamuumeda@gmail.com>
2021-01-21 20:59:11 -0300

    Update desafio009.py

--- Mundo 1 Fundamentos/Desafios/desafio009.py
+++ Mundo 1 Fundamentos/Desafios/desafio009.py
@@ -1,8 +1,9 @@
 numero = int(input('Digite um número inteiro: '))
 mensagem = 'A tabuada do número {} é: \n'
+mensagem += '-' * 12 + '\n'
 
 for i in range(1,10):
     resultado = numero * i
     mensagem = mensagem + str(numero) + 'x' + str(i) + '=' + str(resultado) + '\n'
 
-print(mensagem.format(numero))
+print(mensagem.format(numero)+('-' * 12))

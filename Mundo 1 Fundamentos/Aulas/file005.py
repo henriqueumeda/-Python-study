1b0fe45e918f2e5146e32b16138355bd77c95820
Henrique Umeda <issamuumeda@gmail.com>
2021-01-22 22:45:35 -0300

    Update file005.py

--- Mundo 1 Fundamentos/Aulas/file005.py
+++ Mundo 1 Fundamentos/Aulas/file005.py
@@ -1,4 +1,4 @@
 import math
 num = int(input('Digite um número: '))
 raiz = math.sqrt(num)
-print('A raiz de {}, arredondada para cima, é igual {}'.format(num, math.ceil(raiz)))
+print('A raiz de {} é igual {}\nA raiz de {} arredondada para cima é {}'.format(num, raiz, num, math.ceil(raiz)))

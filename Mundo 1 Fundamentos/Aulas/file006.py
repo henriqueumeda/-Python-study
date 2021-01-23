f10cd612efa4d99084504acb5f74bc03bbba6956
Henrique Umeda <issamuumeda@gmail.com>
2021-01-22 22:46:01 -0300

    Update file006.py

--- Mundo 1 Fundamentos/Aulas/file006.py
+++ Mundo 1 Fundamentos/Aulas/file006.py
@@ -1,4 +1,4 @@
 from math import sqrt, floor
 num = int(input('Digite um número: '))
 raiz = sqrt(num)
-print('A raiz de {} é igual a {}\nA raiz de {} arredondada para baixo é {}'.format(num, raiz, num, floor(raiz)))
+print('A raiz de {} é igual a {:.2f}\nA raiz de {} arredondada para baixo é {}'.format(num, raiz, num, floor(raiz)))

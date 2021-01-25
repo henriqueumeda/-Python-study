687bf64ad036fe1247480491f1567f7f505c2f10
Henrique Umeda <issamuumeda@gmail.com>
2021-01-25 17:12:57 -0300

    Create desafio033.py

---
+++ Mundo 1 Fundamentos/Desafios/desafio033.py
@@ -0,0 +1,6 @@
+primeiroValor = float(input('Primeiro valor: '))
+segundoValor = float(input('Segundo valor: '))
+terceiroValor = float(input('Terceiro valor: '))
+lista = [primeiroValor, segundoValor, terceiroValor]
+print('O menor valor digitado foi {}'.format(min(lista)))
+print('O maior valor digitado foi {}'.format(max(lista)))

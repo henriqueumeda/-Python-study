519c2627966ec5b65196d1da379d58e572853c68
Henrique Umeda <issamuumeda@gmail.com>
2021-01-25 14:50:37 -0300

    Create file010.py

---
+++ Mundo 1 Fundamentos/Aulas/file010.py
@@ -0,0 +1,8 @@
+nome = input('Qual é seu nome? ')
+
+if nome == 'Henrique':
+    print('Que nome lindo você tem!')
+else:
+    print('Seu nome é muito bonito!')
+
+print('Bom dia, {}!'.format(nome))

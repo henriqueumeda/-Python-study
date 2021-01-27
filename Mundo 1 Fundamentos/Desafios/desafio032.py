40024d3b4c98b83af14d36a7027c0eaf7af5513c
Henrique Umeda <issamuumeda@gmail.com>
2021-01-26 21:22:40 -0300

    Update desafio032.py

--- Mundo 1 Fundamentos/Desafios/desafio032.py
+++ Mundo 1 Fundamentos/Desafios/desafio032.py
@@ -1,10 +1,10 @@
 from datetime import date
-ano = int(input('Que ano quer analisar? Coloque 0 para analisar o ano atual: '))
+ano = int(input('Que ano quer analisar? Coloque \033[1;36m0\033[m para analisar o ano \033[1;36matual\033[m: '))
 
 if ano == 0:
     ano = date.today().year
 
 if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
-    print('O ano {} é BISSEXTO'.format(ano))
+    print('O ano \033[1m{}\033[m é \033[4;34mBISSEXTO\033[m'.format(ano))
 else:
-    print('O ano {} não é BISSEXTO'.format(ano))
+    print('O ano \033[1m{}\033[m \033[1;31mNÃO\033[m é \033[4;34mBISSEXTO\033[m'.format(ano))

6f36a027147e28eab1d0ea3600a33cca6135a6c0
Henrique Umeda <issamuumeda@gmail.com>
2021-01-26 21:25:26 -0300

    Update desafio031.py

--- Mundo 1 Fundamentos/Desafios/desafio031.py
+++ Mundo 1 Fundamentos/Desafios/desafio031.py
@@ -1,9 +1,9 @@
-distancia = float(input('Qual é a distância, em Km, da sua viagem? '))
+distancia = float(input('Qual é a \033[4mdistância\033[m, em Km, da sua viagem? '))
 
 if distancia <= 200:
     preco = distancia * 0.5
 else:
     preco = distancia * 0.45
 
-print('Você está prestes a começar uma viagem de {}Km.'.format(distancia))
-print('E o preço da sua passagem será de R${:.2f}'.format(preco))
+print('Você está prestes a começar uma viagem de \033[1;30;47m{}Km\033[m.'.format(distancia))
+print('E o \033[1;4;32mpreço\033[m da sua passagem será de \033[1;4;30;42mR${:.2f}\033[m'.format(preco))

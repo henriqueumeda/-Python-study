0c35a53fa99f933bc3b022e02aaec4af75b671ec
Henrique Umeda <issamuumeda@gmail.com>
2021-01-26 21:34:39 -0300

    Update desafio028.py

--- Mundo 1 Fundamentos/Desafios/desafio028.py
+++ Mundo 1 Fundamentos/Desafios/desafio028.py
@@ -1,11 +1,11 @@
 from random import randint
 from time import sleep
-print('-=-' * 20 + '\nVou pensar em um número entre 0 e 5. Tente adivinhar...\n' + '-=-' * 20)
+print('\033[33m-=-' * 20 + '\nVou pensar em um número entre 0 e 5. Tente adivinhar...\n' + '-=-' * 20 + '\033[m')
 numeroUser = int(input('Em que número pensei? '))
 numeroPC = randint(0, 5)
 print('PROCESSANDO...')
 sleep(2)
 if numeroUser == numeroPC:
-    print('PARABÉNS! Você conseguiu me vencer!')
+    print('\033[1;36mPARABÉNS!\033[m Você conseguiu me vencer!')
 else:
-    print('GANHEI! Eu pensei no número {} e não no {}!'.format(numeroPC, numeroUser))
+    print('\033[1;31mGANHEI!\033[m Eu pensei no número \033[1;4;31m{}\033[m e não no \033[1;37m{}\033[m!'.format(numeroPC, numeroUser))

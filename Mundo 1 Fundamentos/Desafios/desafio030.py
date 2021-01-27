22c495251073e8f575cf036e1e79eaf2f17dfa09
Henrique Umeda <issamuumeda@gmail.com>
2021-01-26 21:27:06 -0300

    Update desafio030.py

--- Mundo 1 Fundamentos/Desafios/desafio030.py
+++ Mundo 1 Fundamentos/Desafios/desafio030.py
@@ -1,5 +1,5 @@
-numero = int(input('Digite um número inteiro: '))
+numero = int(input('\033[33mDigite um número inteiro:\033[m '))
 if numero % 2 == 0:
-    print('O número {} é PAR!'.format(numero))
+    print('O número {} é \033[1;36mPAR\033[m!'.format(numero))
 else:
-    print('O número {} é ÍMPAR!'.format(numero))
+    print('O número {} é \033[1;31mÍMPAR\033[m!'.format(numero))

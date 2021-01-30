648cff29061b2078bca0b6527cdd0db3c481a551
Henrique Umeda <issamuumeda@gmail.com>
2021-01-30 12:41:48 -0300

    Update desafio055_b.py

--- Mundo 2 Estruturas de Controle/Desafios/desafio055_b.py
+++ Mundo 2 Estruturas de Controle/Desafios/desafio055_b.py
@@ -4,5 +4,5 @@ for contador in range(1,6):
     peso = float(input('Peso da {}ª pessoa: '.format(contador)))
     lista += [peso]
 
-print('O maior peso da lista foi {}'.format(max(lista)))
-print('O menor peso da lista foi {}'.format(min(lista)))
+print('O maior peso da lista foi {}Kg'.format(max(lista)))
+print('O menor peso da lista foi {}Kg'.format(min(lista)))

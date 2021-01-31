b474d53731a6a212353e751687d0145bfd898368
Henrique Umeda <issamuumeda@gmail.com>
2021-01-31 18:02:45 -0300

    Create desafio057.py

---
+++ Mundo 2 Estruturas de Controle/Desafios/desafio057.py
@@ -0,0 +1,6 @@
+sexo = input("Informe seu sexo [M/F]: ").strip().upper()[0]
+
+while sexo not in 'MF':
+    sexo = input('Dados inválidos. Por favor, informe seu sexo [M/F]: ').strip().upper()[0]
+
+print('Sexo {} registrado com sucesso!'.format(sexo))

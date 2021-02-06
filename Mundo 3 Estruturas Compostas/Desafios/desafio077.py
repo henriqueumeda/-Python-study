15cedd8bbbfd9d9c491cbcd5f603024760829d67
Henrique Umeda <issamuumeda@gmail.com>
2021-02-06 13:27:44 -0300

    Create desafio077.py

---
+++ Mundo 3 Estruturas Compostas/Desafios/desafio077.py
@@ -0,0 +1,8 @@
+lista_palavras = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSO', 'GRATIS', 'ESTUDAR',
+            'PRATICAR', 'TRABALHAR', 'MERCADO', 'PROGRAMADOR', 'FUTURO')
+
+for palavra in lista_palavras:
+    print(f'\nNa palavra {palavra} temos ', end='')
+    for letra in palavra:
+        if letra in 'AEIOU':
+            print(f'{letra.lower()}', end=' ')

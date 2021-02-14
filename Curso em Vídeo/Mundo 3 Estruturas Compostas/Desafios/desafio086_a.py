5fb2110b36132c1a1d3e627e4e4c6e0bccb72e5e
Henrique Umeda <issamuumeda@gmail.com>
2021-02-13 23:49:19 -0300

    Create desafio086_a.py

---
+++ Curso em Vídeo/Mundo 3 Estruturas Compostas/Desafios/desafio086_a.py
@@ -0,0 +1,11 @@
+matrix = []
+for line in range(0, 3):
+    for column in range(0, 3):
+        number = int(input(f'Input a number to the position [{line}:{column}]: '))
+        matrix.append([line, column, number])
+print('-=' * 15, end='')
+
+for counter in range(0, 9):
+    if matrix[counter][0] != matrix[counter-1][0]:
+        print('')
+    print(f'[ {matrix[counter][2]:3} ]', end='')

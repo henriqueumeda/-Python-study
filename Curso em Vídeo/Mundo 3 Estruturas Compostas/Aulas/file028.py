76ecfe4dce12b893003b6773aa5e92f7b9834d4d
Henrique Umeda <issamuumeda@gmail.com>
2021-02-20 20:29:13 -0300

    Create file028.py

---
+++ Curso em Vídeo/Mundo 3 Estruturas Compostas/Aulas/file028.py
@@ -0,0 +1,8 @@
+brasil = []
+state1 = {'uf': 'Rio de Janeiro', 'acronym': 'RJ'}
+state2 = {'uf': 'São Paulo', 'acronym': 'SP'}
+brasil.append(state1)
+brasil.append(state2)
+print(brasil)
+print(brasil[0]['uf'])
+print(brasil[1]['acronym'])

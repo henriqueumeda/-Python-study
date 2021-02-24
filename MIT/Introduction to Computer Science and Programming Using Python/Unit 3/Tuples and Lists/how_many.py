c9fa9445273774957ba687e1a5598a292deead16
Henrique Umeda <issamuumeda@gmail.com>
2021-02-23 21:35:33 -0300

    Create how_many.py

---
+++ MIT/Introduction to Computer Science and Programming Using Python/Unit 3/Tuples and Lists/how_many.py
@@ -0,0 +1,19 @@
+def how_many(aDict):
+    '''
+    aDict: A dictionary, where all the values are lists.
+
+    returns: int, how many values are in the dictionary.
+    '''
+    # Your Code Here
+    counter = 0
+    for value in aDict.values():
+        counter += len(value)
+    return counter
+
+animals = { 'a': ['aardvark'], 'b': ['baboon'], 'c': ['coati']}
+
+animals['d'] = ['donkey']
+animals['d'].append('dog')
+animals['d'].append('dingo')
+
+print(how_many(animals))

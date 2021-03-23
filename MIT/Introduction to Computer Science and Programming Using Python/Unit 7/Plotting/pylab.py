1a26eb135165bf4b3e350096a33f149eef9de0c1
Henrique Umeda <issamuumeda@gmail.com>
2021-03-22 23:55:57 -0300

    Create pylab.py

---
+++ MIT/Introduction to Computer Science and Programming Using Python/Unit 7/Plotting/pylab.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 22 22:43:37 2021
+
+@author: Issamu Umeda
+"""
+import pylab as plt
+
+mySamples = []
+myLinear = []
+myQuadratic = []
+myCubic = []
+myExponential = []
+
+for i in range(0, 30):
+    mySamples.append(i)
+    myLinear.append(i)
+    myQuadratic.append(i**2)
+    myCubic.append(i**3)
+    myExponential.append(1.5**i)
+
+plt.figure('lin')
+plt.clf()
+plt.title('Linear')
+#plt.xlabel('sample points')
+#plt.ylabel('linear function')
+plt.plot(mySamples, myLinear)
+plt.figure('quad')
+plt.clf()
+plt.title('Quadratic')
+plt.plot(mySamples, myQuadratic)
+plt.figure('cube')
+plt.clf()
+plt.title('Cubic')
+plt.plot(mySamples, myCubic)
+plt.figure('expo')
+plt.clf()
+plt.title('Exponential')
+plt.plot(mySamples, myExponential)
+#plt.figure('quad')
+#plt.ylabel('quadratic function')

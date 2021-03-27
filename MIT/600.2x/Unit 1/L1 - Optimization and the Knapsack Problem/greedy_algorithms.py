e157221b852d141a865fabd64693082419a5c9e2
Henrique Umeda <issamuumeda@gmail.com>
2021-03-26 21:51:36 -0300

    Update greedy_algorithms.py

--- MIT/600.2x/Unit 1/L1 - Optimization and the Knapsack Problem/greedy_algorithms.py
+++ MIT/600.2x/Unit 1/L1 - Optimization and the Knapsack Problem/greedy_algorithms.py
@@ -60,8 +60,38 @@ def testGreedys(foods, maxUnits):
     print('\nUse greedy by density to allocate', maxUnits, 'calories')
     testGreedy(foods, maxUnits, Food.density)
 
+def maxVal(toConsider, avail):
+    """Assumes toConsider a list of items,
+    avail a weight
+    Returns a tuple of the total value of a solution to 0/1 knapsack problem
+    and the items of that solution"""
+    if toConsider == [] or avail == 0:
+        result = (0, ())
+    elif toConsider[0].getCost() > avail:
+        result = maxVal(toConsider[1:], avail)
+    else:
+        nextItem = toConsider[0]
+        withVal, withToTake = maxVal(toConsider[1:], avail-nextItem.getCost())
+        withVal += nextItem.getValue()
+        withoutVal, withoutToTake = maxVal(toConsider[1:], avail)
+        if withVal > withoutVal:
+            result = (withVal, withToTake + (nextItem,))
+        else:
+            result = (withoutVal, withoutToTake)
+    return result
+
+def testMaxVal(foods, maxUnits, printItems = True):
+    print('Use search tree to allocate', maxUnits, 'calories')
+    val, taken = maxVal(foods, maxUnits)
+    print('Total value of items taken =', val)
+    if printItems:
+        for item in taken:
+            print(' ', item)
+
 names = ['wine', 'beer', 'pizza', 'burger', 'fries', 'cola', 'apple', 'donut', 'cake']
 values = [89, 90, 95, 100, 90, 79, 50, 10]
 calories = [123, 154, 258, 354, 365, 150, 95, 195]
 foods = buildMenu(names, values, calories)
-testGreedys(foods, 1000)
+testGreedys(foods, 750)
+print()
+testMaxVal(foods, 750)
